b992f2efebe73e277657d83c3ce488b28b931aa9
Briana Davis <brianad570@gmail.com>
2026-03-05 11:41:48 -0500

    Added 3 pages to The Phalanx page

--- IEEEWebsite/src/pages/Phalanx.jsx
+++ IEEEWebsite/src/pages/Phalanx.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import PhalanxHomePage from './The Phalanx/PhalanxHomePage'
+import TrojanBeginning from './The Phalanx/TrojanBeginning'
+import BattlePlanVision from './The Phalanx/BattlePlanVision'
 
 const Phalanx = () => {
   return (
     <div>
         <PhalanxHomePage/>
+        <TrojanBeginning/>
+        <BattlePlanVision/>
         
     </div>
   )
